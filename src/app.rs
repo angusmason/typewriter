3412a5d19a90f1de87d0c63157a2ed1844d0d64b
Angus Mason <angus.mason@me.com>
2024-10-08 16:06:12 +1100

    giggle gang

--- src/app.rs
+++ src/app.rs
@@ -2,11 +2,15 @@ use leptos::*;
 
 #[component]
 pub fn App() -> impl IntoView {
-
-
     view! {
+        <div>
+            <div class="absolute top-0 h-30 w-full bg-slate-500" data-tauri-drag-region />
+        </div>
         <main class="container">
-            <textarea contenteditable="true"></textarea>
+            <div>
+                <div class="absolute top-0 h-30 w-full bg-slate-500" data-tauri-drag-region />
+            </div>
+            <textarea contenteditable="true" style="color: #FFFFFF; background-color:#29272B"></textarea>
         </main>
     }
 }
