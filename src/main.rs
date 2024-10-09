2b5fc23182c3952b5468751df874170ae060e175
Angus Mason <angus.mason@me.com>
2024-10-09 14:23:00 +1100

    remove unused font

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ pub fn App() -> impl IntoView {
             <div data-tauri-drag-region class="fixed top-0 z-10 w-full h-8 cursor-grab" />
             <main class="flex flex-col justify-start pt-10 grow bg-brown">
                 <div
-                    class="w-full h-full px-16 pt-4 font-mono text-base text-white outline-none caret-white bg-brown"
+                    class="overscroll-none w-full h-full px-16 pt-4 font-mono text-base text-white outline-none caret-white bg-brown"
                     contenteditable="true"
                     spellcheck="false"
                 ></div>
