6252c0430b366180a68ad7fcc147284b45508f2f
Matthew Li <matthew.li10@education.nsw.gov.au>
2024-10-09 13:41:54 +1100

    Fix styles

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,9 @@ pub fn App() -> impl IntoView {
     view! {
         <div class="flex flex-col h-full bg-brown">
             <div data-tauri-drag-region class="fixed top-0 z-10 w-full bg-white h-30 cursor-grab" />
-            <main class="flex flex-col justify-start flex-grow pt-10 m-0 bg-brown">
+            <main class="flex flex-col justify-start pt-10 grow bg-brown">
                 <div
-                    class="w-full h-full pt-4 pl-16 pr-16 font-mono text-lg text-white border-none outline-none caret-white bg-brown"
+                    class="w-full h-full px-16 pt-4 font-mono text-lg text-white outline-none caret-white bg-brown"
                     contenteditable="true"
                     spellcheck="false"
                 ></div>
