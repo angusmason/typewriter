c9f74fb36d959dea5543f32da4254f2d3fd5b4d2
Angus Mason <angus.mason@me.com>
2024-10-09 12:38:19 +1100

    humpty dumpty

--- src/app.rs
+++ src/app.rs
@@ -4,9 +4,9 @@ use leptos::*;
 pub fn App() -> impl IntoView {
     view! {
         <div class="h-full flex flex-col">
-            <div class="absolute top-0 h-30 w-full bg-slate-500 cursor-grab z-10" data-tauri-drag-region />
+            <div data-tauri-drag-region class="fixed top-0 h-30 w-full bg-white cursor-grab z-10" />
             <main class="bg-brown flex-grow m-0 pt-10 flex flex-col justify-start">
-                <div 
+                <div
                     class="font-mono text-lg text-white caret-white bg-brown border-none outline-none pl-16 pt-4 pr-16 w-full h-full"
                     contenteditable="true"
                     spellcheck="false"
