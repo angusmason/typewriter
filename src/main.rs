cda0565a49a0fcaee3e4ec175dddd4fa6f647ebb
Matthew Li <matthew.li10@education.nsw.gov.au>
2024-10-09 15:44:12 +1100

    Refactor logging macro to use leptos logging and update textarea class for improved styling

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 #![warn(clippy::pedantic, clippy::nursery)]
 use console_error_panic_hook::set_once;
 use leptos::{component, create_rw_signal, event_target_value, IntoView, SignalSet};
-use leptos::{logging::log, mount_to_body, view};
+use leptos::{mount_to_body, view};
 use unicode_segmentation::UnicodeSegmentation;
 
+#[allow(unused_macros)]
 macro_rules! dbg {
     () => {
-        log!("[{}:{}:{}]", file!(), line!(), column!())
+        leptos::logging::log!("[{}:{}:{}]", file!(), line!(), column!())
     };
     ($val:expr $(,)?) => {
         match $val {
             tmp => {
-                log!("[{}:{}:{}] {} = {:#?}",
+                leptos::logging::log!("[{}:{}:{}] {} = {:#?}",
                     file!(), line!(), column!(), stringify!($val), &tmp);
                 tmp
             }
@@ -37,7 +38,7 @@ pub fn App() -> impl IntoView {
         <div class="flex flex-col h-full text-white bg-brown caret-white [&_*]:[font-synthesis:none]">
             <div data-tauri-drag-region class="w-full h-8 cursor-grab" />
             <textarea
-                class="p-4 px-16 text-sm bg-transparent outline-none size-full selection:bg-darkbrown overscroll-none"
+                class="p-4 px-16 text-sm bg-transparent outline-none resize-none size-full selection:bg-darkbrown"
                 prop:value=text
                 on:input=move |event| {
                     text.set(event_target_value(&event));
