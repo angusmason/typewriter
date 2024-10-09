14ab37d4b45476ea9f79744626332a3b5088881b
Matthew Li <matthew.li10@education.nsw.gov.au>
2024-10-09 13:13:12 +1100

    Update README and clean up HTML/CSS structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "index.html",
-      "src/**/*.rs",
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          'mono': ["Mono"],
-        },
-        colors: {
-            'brown': '#29272B',
-          },
+  content: [
+    "index.html",
+    "src/**/*.rs",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'mono': ["Mono"],
+      },
+      colors: {
+        'brown': '#29272B',
       },
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+}
